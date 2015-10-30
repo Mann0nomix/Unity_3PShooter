fdd5bb394d81640e82e9795871c853af209ce6c4
Edj3 <ejmann@udel.edu>
2015-10-29 20:33:04 -0400

    Lets see if i can upload all these files

---
+++ Assets/ChanMovement.cs
@@ -0,0 +1,97 @@
+using UnityEngine;
+using System.Collections;
+
+public class ChanMovement : MonoBehaviour {
+    [SerializeField]
+    private Rigidbody rb;
+    [SerializeField]
+    private float speed = 5f;
+    static private Animator anim;
+    
+    private float moveX;
+    private float moveZ;
+    static private bool sheJumps;
+    static private bool dash;
+    static private bool goDown;
+    static private bool landed;
+    static private bool walking;
+    static private bool running;
+    static private bool disableDash;
+
+    // Use this for initialization
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        moveX = Input.GetAxis("Horizontal");
+        //move backwards should be half the speed
+        moveZ = Mathf.Clamp(Input.GetAxis("Vertical"), -0.5f, 1f);
+
+        //set the float values in the animator controller to the values detected by the input device the client is using
+        anim.SetFloat("moveX", moveX);
+        anim.SetFloat("moveZ", moveZ);
+
+        //If you are holdin down the key = getkey
+        sheJumps = Input.GetKeyDown("space");
+        dash = Input.GetKeyDown("return");
+
+        //only can dash for 2 seconds
+        if (dash && !disableDash) {
+            StartCoroutine(TimeDash());
+        }
+    }
+
+    void FixedUpdate() {
+        //calculate vector for movement
+        Vector3 movementVec = new Vector3(moveX, 0, moveZ);
+        
+        //dash functionality else normal movement
+        if (anim.GetBool("dash")) {
+            rb.MovePosition(rb.position + Vector3.forward * (speed * 3) * Time.fixedDeltaTime);   //Dash = move forward 3x as fast
+        } else {
+            rb.MovePosition(rb.position + movementVec * speed * Time.deltaTime);
+        }
+
+        Jump();
+    }
+
+    void Jump() {
+        //need to do this because the other way does not work with the booleans in C#
+        if (sheJumps) {
+            rb.MovePosition(rb.position + Vector3.up * speed * Time.fixedDeltaTime);
+            anim.SetBool("jump", true);
+        }
+
+        //if you have slightly left the ground, continue moving upward until you hit 8 points in the y direction, otherwise, come back down
+        if (!landed && rb.position.y > .01) {
+            rb.MovePosition(rb.position + Vector3.up * speed * Time.fixedDeltaTime);
+            if (rb.position.y > 8) {
+                goDown = true;
+            }
+            if (goDown) {
+                rb.MovePosition(rb.position + Vector3.down * Time.fixedDeltaTime / 4);
+                goDown = false;
+
+                if (rb.position.y < 1) {
+                    landed = true;
+                }
+            }
+        } else {
+            anim.SetBool("jump", false);
+        }
+    }
+
+    IEnumerator TimeDash() {
+        anim.SetBool("dash", true);
+        yield return new WaitForSeconds(1f);
+        anim.SetBool("dash", false);
+        disableDash = true;
+
+        //wait another 2 seconds before being able to perform another dash
+        yield return new WaitForSeconds(2f);
+        disableDash = false;
+    }
+}
---
+++ Assets/FireGun.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+public class FireGun : MonoBehaviour {
+    LineRenderer gunLine;
+    ParticleSystem gunParticles;
+    Light gunLight;
+    AudioSource gunSound;
+    LineRenderer gunRender;
+
+    Ray shotRay;
+    RaycastHit shotHit;
+
+    float gunRange = 100f;
+    float effectDisplayTime = 0.2f;
+    float timeBetweenShots = 0.1f;
+    float timePassed;
+
+	// Use this for initialization
+	void Start () {
+        //get all components that resemble firing of gun
+        gunLine = GetComponent<LineRenderer>();
+        gunParticles = GetComponent<ParticleSystem>();
+        gunLight = GetComponent<Light>();
+        gunSound = GetComponent<AudioSource>();
+        gunRender = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timePassed += Time.deltaTime;
+        //detect if the user hits the enter key, and the time between shots then fire
+        if (Input.GetKeyDown(KeyCode.Z) && timePassed >= timeBetweenShots && Time.timeScale != 0) {
+            Shoot();
+        }
+
+        //if time passes while effects are on, turn them off based on effectDisplaytime
+        if(timePassed >= timeBetweenShots * effectDisplayTime) {
+            DisableEffects();
+        }
+	}
+
+    void Shoot() {
+        //reset timer
+        timePassed = 0f;
+
+        //play the gun shot sound
+        gunSound.Play();
+
+        //Stop particles if they were playing, then restart particles for shot
+        //gunParticles.Stop();
+        //gunParticles.Play();
+
+        //enable light and renderer
+        gunLight.enabled = true;
+        gunLine.enabled = true;
+
+        //set up ray so that it starts at the end of the barrel and points forward
+        shotRay.origin = transform.position;
+        shotRay.direction = transform.forward;
+
+        //handle raycast logic to draw line
+        if (Physics.Raycast(shotRay, out shotHit)) {
+            gunLine.SetPosition(1, shotHit.point);
+        } else {
+            gunLine.SetPosition(1, new Vector3(0, 0, 100));
+        }
+    }
+
+    void DisableEffects() {
+        gunLight.enabled = false;
+        gunLine.enabled = false;
+    }
+}
